aa1d387e1d44a16460f2ed6e1a5691df4cc7cd59
Nelber Mora <nelbermora@gmail.com>
2024-05-13 13:42:44 -0300

    fixing hours

--- src/app/pipes/dateFull.pipe.ts
+++ src/app/pipes/dateFull.pipe.ts
@@ -5,7 +5,7 @@ import { Pipe, PipeTransform } from "@angular/core";
     name: 'dateFull'
 })
 export class DateFullPipe extends DatePipe implements PipeTransform{
-    transform(strDate: string): any {
+    /*transform(strDate: string): any {
         //2021-09-05 19:00:00
         let newDate = new Date();
         newDate.setFullYear(+strDate.substring(0,4));
@@ -13,6 +13,25 @@ export class DateFullPipe extends DatePipe implements PipeTransform{
         newDate.setDate(+strDate.substring(8,10));
         newDate.setHours((+strDate.substring(11,13)),+strDate.substring(14,16))
         return super.transform(newDate, "dd/MM/yyyy h:mm a");
+    }*/
+    transform(fechaHora: string): string {
+        const fechaHoraLocal = new Date(fechaHora);
+        const offset = fechaHoraLocal.getTimezoneOffset();
+    
+        fechaHoraLocal.setMinutes(fechaHoraLocal.getMinutes() - offset);
+        const opcionesDeFormato: Intl.DateTimeFormatOptions = {
+            day: '2-digit',
+            month: '2-digit',
+            year: 'numeric',
+            hour: '2-digit',
+            minute: '2-digit',
+            hour12: true
+          };
+      
+        const fechaHoraFormateada = fechaHoraLocal.toLocaleString('es-ES', opcionesDeFormato);
+        //const fechaHoraFormateada = fechaHoraLocal.toLocaleString();
+    
+        return fechaHoraFormateada;
     }
     
 }
